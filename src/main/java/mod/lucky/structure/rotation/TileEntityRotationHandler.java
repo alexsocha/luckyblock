519f9902bcd2a9af6e4a4a41e3c377ab3d519924
alexsocha <alex@alexsocha.com>
2019-04-19 12:32:46 +1000

    Initial 1.13 commit

--- src/main/java/mod/lucky/structure/rotation/TileEntityRotationHandler.java
+++
@@ -1,7 +0,0 @@
-package mod.lucky.structure.rotation;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityRotationHandler {
-  public void rotate(TileEntity tileEntity, int rotation) {}
-}
