97c7bae642e403f917be6ef317a11fa6b66c6140
alexsocha <alex@alexsocha.com>
2020-02-09 15:58:30 +1100

    build: create separate dist dir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 script_dir=$(dirname $0)
-dist_dir=$script_dir/../build/libs
+dist_dir=$script_dir/../build/dist
 secret_folder=jKEbJUId
 deploy_dir=/var/www/html/projects/lucky_block/download/version/files/$secret_folder
 
-find $dist_dir -regex ".*/luckyblock-.*[0-9]-.*[0-9]\.jar" -exec \
+find $dist_dir -regex ".*\.jar" -exec \
   scp {} root@minecraftascending.com:$deploy_dir \;
