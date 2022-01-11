f2460755b7109b697642d6700bf74f2d993256d4
alexsocha <alex@alexsocha.com>
2022-01-11 22:51:29 +1100

    feat(fabric): support for fabric-loader >=0.12.0

--- fabric/src/main/kotlin/FabricJavaGameAPI.kt
+++ fabric/src/main/kotlin/FabricJavaGameAPI.kt
@@ -8,6 +8,7 @@ import mod.lucky.fabric.*
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.impl.FabricLoaderImpl
 import net.minecraft.block.Blocks
 import net.minecraft.block.entity.ChestBlockEntity
 import net.minecraft.client.MinecraftClient
@@ -49,16 +50,16 @@ object FabricJavaGameAPI : JavaGameAPI {
     }
 
     override fun getModVersion(): String {
-        return (FabricLoader.getInstance() as net.fabricmc.loader.FabricLoader).getModContainer("lucky")
+        return FabricLoader.getInstance().getModContainer("lucky")
             .get().metadata.version.friendlyString
 
     }
     override fun getMinecraftVersion(): String {
-        return (FabricLoader.getInstance() as net.fabricmc.loader.FabricLoader).gameProvider.normalizedGameVersion
+        return (FabricLoader.getInstance() as FabricLoaderImpl).gameProvider.normalizedGameVersion
     }
 
     override fun getGameDir(): File {
-        return (FabricLoader.getInstance() as net.fabricmc.loader.FabricLoader).gameProvider.launchDirectory.toFile()
+        return FabricLoader.getInstance().gameDir.toFile()
     }
 
     override fun attrToNBT(attr: Attr): Tag {
