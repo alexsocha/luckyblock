59211d5f3a8c46fe5b05e7affcc0489b54c1f387
alexsocha <alex@alexsocha.com>
2022-03-27 22:56:40 +1100

    fix: include lucky-config.zip in final jar

--- buildSrc/src/main/kotlin/JavaEditionTasks.gradle.kts
+++ buildSrc/src/main/kotlin/JavaEditionTasks.gradle.kts
@@ -22,7 +22,7 @@ tasks.register<Copy>("copyRuntimeResources") {
 tasks.register<Zip>("luckyBlockConfigDist") {
     archiveFileName.set("lucky-config.zip")
     destinationDirectory.set(file("$rootDir/common/build/tmp"))
-    from("src/jvmMain/resources/lucky-config")
+    from("$rootDir/common/src/jvmMain/resources/lucky-config")
 }
 
 tasks.register<Zip>("exportDist") {
