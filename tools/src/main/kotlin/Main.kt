9d73908ab2cca96b433d298468231d4c364875dc
alexsocha <alex@alexsocha.com>
2023-09-03 20:31:53 +1000

    chore: remove curseforge deployment

--- tools/src/main/kotlin/Main.kt
+++ tools/src/main/kotlin/Main.kt
@@ -2,13 +2,10 @@
 
 package mod.lucky.tools
 
-import mod.lucky.tools.uploadToCurseForge.UploadToCurseForge
 import kotlinx.cli.*
 
 fun main(args: Array<String>) {
     val parser = ArgParser("luckytools", strictSubcommandOptionsOrder = true)
-
-    parser.subcommands(GenerateBedrockDrops(), NbtToMcstructure(), DownloadBlockIds(), UploadToCurseForge())
-
+    parser.subcommands(GenerateBedrockDrops(), NbtToMcstructure(), DownloadBlockIds())
     parser.parse(args)
 }
