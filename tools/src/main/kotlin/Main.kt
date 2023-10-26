2db4863dc764f6d27eb062cb4cd4907a4fa961cf
alexsocha <alex@alexsocha.com>
2023-10-26 16:06:31 +1100

    Revert "chore: remove curseforge deployment"
    This reverts commit 9d73908ab2cca96b433d298468231d4c364875dc.

--- tools/src/main/kotlin/Main.kt
+++ tools/src/main/kotlin/Main.kt
@@ -2,10 +2,13 @@
 
 package mod.lucky.tools
 
+import mod.lucky.tools.uploadToCurseForge.UploadToCurseForge
 import kotlinx.cli.*
 
 fun main(args: Array<String>) {
     val parser = ArgParser("luckytools", strictSubcommandOptionsOrder = true)
-    parser.subcommands(GenerateBedrockDrops(), NbtToMcstructure(), DownloadBlockIds())
+
+    parser.subcommands(GenerateBedrockDrops(), NbtToMcstructure(), DownloadBlockIds(), UploadToCurseForge())
+
     parser.parse(args)
 }
